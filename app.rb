74125a01fc437497f81bb629f7a0786c51f0481a
richardcrng@gmail.com <richardcrng@gmail.com>
2019-08-13 15:41:46 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    'Hello, World!'
+  end
+
+  get '/name' do
+    'My name is Richard'
+  end
+
+  get '/hometown' do
+    'My hometown is Sutton Coldfield'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is We Are Young'
+  end
 end
